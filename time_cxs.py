635a0005f86b7927ab1c0662aa449e52062cba18
Maximilian Press <maximilianpress5@gmail.com>
2019-05-13 23:06:15 -0700

    fixed a bug in handling timedeltas.

--- time_cxs.py
+++ time_cxs.py
@@ -33,7 +33,6 @@ class CxRecord(object):
 		pickle.dump(self, file=pkl_file)
 		
 	def compute_stats(self):
-		print("computing stats")
 		self.median_length = compute_median(self.lengths)
 		self.diffs = diff_time_series(self.begins)
 		self.median_interval = compute_median(self.diffs)
@@ -58,7 +57,7 @@ class CxRecord(object):
 			it+=1
 			print("{0} contractions measured".format(it))
 		
-		self.lengths = cx_lengths
+		self.lengths = [cx_length.total_seconds() for cx_length in cx_lengths]
 		self.begins = cx_begins
 		self.ends = cx_ends
 		self.compute_stats()
@@ -83,28 +82,6 @@ def time_cx():
 	if input == "next":
 		return None, None
 	return begin, end
-
-	
-def run_cx_loop():
-	'''Iterate on contractions within an episode until done with episode.
-	'''
-	cx_lengths = []
-	cx_begins = []
-	it = 0
-	while True:
-		cx_begin, cx_end = time_cx()
-		if str(cx_end).lower() == "done":
-			break
-		elif str(cx_end).lower() is None:
-			continue
-		else:
-			cx_lengths.append(cx_delta)
-			cx_delta = cx_end - cx_begin
-		
-		cx_begins.append(cx_begin)
-		it+=1
-		print("{0} contractions measured".format(it))
-	return cx_lengths, cx_begins
 	
 def diff_time_series(cx_begins):
 	'''Diff the beginning times of an episode's beginning time measurements.
@@ -135,17 +112,6 @@ def compute_median(list):
 		median = list[int(midpoint)]
 	return median
 	
-def compute_cx_stats(cx_lengths, cx_begins):
-	'''Given expected contraction time measurements, compute stats of interest.
-	'''
-	cx_sec_lens = [cx.total_seconds() for cx in cx_lengths]
-	median_cx = compute_median(cx_sec_lens)
-	diffs = diff_time_series(cx_begins)
-	median_diffs = compute_median(diffs)
-	print("median interval between cx", median_diffs)
-	print("median length of cx", median_cx)
-	return median_cx, median_diffs, diffs
-
 def write_cx_data_to_log(cx_begins, cx_lengths, logfile = "cx_log.txt"):
 	'''Write raw contraction data to human-readable log.
 	'''
@@ -155,13 +121,6 @@ def write_cx_data_to_log(cx_begins, cx_lengths, logfile = "cx_log.txt"):
 											   str(cx_begins[i]).replace(" ", "_"),
 											   cx_lengths[i])
 			out.write(log_str)
-	
-def make_cx_record():
-	'''Run the procedures to get info on a contraction episode.
-	'''
-	cx_lengths, cx_begins = run_cx_loop()
-	median_cx, median_diffs, diffs = compute_cx_stats(cx_lengths, cx_begins)
-	write_cx_data_to_log(cx_begins=cx_begins, cx_lengths=cx_lengths)
 	
 def parse_args():
 	parser = argparse.ArgumentParser(description='contraction timer.')
